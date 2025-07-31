4cd0f3519df3fbe89f26c8257bd76e053ab282f2
icon-13 <jimmymjema@gmail.com>
2025-07-31 13:37:24 +0300

    Update config to use SQLite in instance folder and add local DB file

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_to_a_secure_key'
-    
-    # ✅ Use SQLite by default, PostgreSQL if available
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-    
+
+    # Point to the SQLite DB inside the instance folder
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'instance', 'database.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
