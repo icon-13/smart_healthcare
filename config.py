48b40cf79d2f4db2ac76a993d98936678b724bef
icon-13 <jimmymjema@gmail.com>
2025-07-31 13:24:15 +0300

    Switch to SQLite: add db.create_all() and include local db.sqlite3 file

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'change_this_to_a_secure_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'change_this_to_a_secure_key'
+    
+    # ✅ Use SQLite by default, PostgreSQL if available
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = True
